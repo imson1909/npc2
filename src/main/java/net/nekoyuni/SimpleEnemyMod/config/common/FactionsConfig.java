463ba1759c1842ccbd3006968b54871fbdee5691
imson <236419588+imson1909@users.noreply.github.com>
2026-07-06 16:14:19 +0600

    Delete src/main directory

--- src/main/java/net/nekoyuni/SimpleEnemyMod/config/common/FactionsConfig.java
+++
@@ -1,9 +0,0 @@
-package net.nekoyuni.SimpleEnemyMod.config.common;
-
-import net.minecraftforge.common.ForgeConfigSpec.Builder;
-
-public class FactionsConfig {
-   public static void init(Builder builder) {
-      // No faction configs - only PMC Unit exists
-   }
-}
--- src/main/java/net/nekoyuni/SimpleEnemyMod/entity/unit/util/FactionType.java
+++
@@ -1,6 +0,0 @@
-package net.nekoyuni.SimpleEnemyMod.entity.unit.util;
-
-public enum FactionType {
-    TOWER,
-    BUNKER
-}
